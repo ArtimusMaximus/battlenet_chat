e4f2887b77d877731cb20dad7712b178da997e53
artimusmaximus <arthurmodymaniv@gmail.com>
2024-06-21 13:18:04 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 9999;
+
+app.use(express.static(join(__dirname, '../dist')));
+app.use(cors());
+let allUsers = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, "../dist/index.html"))
+});
+io.on('connection', (socket) => {
+    let username = null;
+    console.log("a user connected...");
+    let channelName = 'Brood War USA-1';
+    socket.join(channelName);
+    socket.emit('joining', channelName);
+
+    socket.on('set username', (name) => {
+        username = name;
+        allUsers.push(username);
+        socket.emit('set username', name);
+        socket.emit('get all users', allUsers);
+    });
+
+    socket.on('chat message', (msg) => {
+        // io.emit('chat message', msg);
+        if (msg.value.startsWith("/join")) {
+            const newChannelName = msg.value.split(" ")[1];
+            if (newChannelName) {
+                socket.leave(channelName);
+                channelName = newChannelName;
+                socket.join(channelName);
+                socket.emit('joining', channelName);
+            }
+            console.log('channelName\t', channelName);
+
+            // io.to(channelName).emit('joinedChannel', `You have joined channel: ${channelName}`);
+        } else {
+            const fullMessage = { name: username, value: msg.value };
+            io.to(channelName).emit('chat message', fullMessage);
+        }
+    });
+});
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server on PORT ${PORT}~~~~`)
+})
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'chat-font': ['Verdana', 'Geneva', 'sans-serif']
+      },
+      backgroundImage: {
+        'battlenet': "url('/battle_net_frame.png')",
+        'battlenet800': "url('/battle_net_frame_800_600_3.png')",
+        'battlenet1024': "url('/battle_net_frame_1024.png')"
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["wireframe", "business", "light", "dark"]
+  }
+}
