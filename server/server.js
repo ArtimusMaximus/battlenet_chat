7949fc9e9e62852454dcf0529616093b19eda4ef
artimusmaximus <arthurmodymaniv@gmail.com>
2024-06-21 17:45:30 -0700

    live version suddenly stopped working, recloning and redeploying

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
         credentials: true
     }
 });
-const PORT = process.env.PORT || 9999;
+const PORT = 9999;
 
 app.use(express.static(join(__dirname, '../dist')));
 app.use(cors({
@@ -24,6 +24,7 @@ app.use(cors({
     credentials: true
 }));
 let allUsers = [];
+const whoIsWhere = {};
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, "../dist/index.html"))
@@ -33,24 +34,28 @@ io.on('connection', (socket) => {
     console.log("a user connected...");
     let channelName = 'Brood War USA-1';
     socket.join(channelName);
-    socket.emit('joining', channelName);
+    socket.emit('joining channel', channelName);
 
     socket.on('set username', (name) => {
         username = name;
-        allUsers.push(username);
+        // allUsers.push(username);
+        allUsers.push({ username, inChannel: channelName });
+        console.log(socket.id)
+        socket.emit('get current channel', channelName);
         socket.emit('set username', name);
         socket.emit('get all users', allUsers);
     });
 
     socket.on('chat message', (msg) => {
         // io.emit('chat message', msg);
+        console.log('msg\t', msg);
         if (msg.value.startsWith("/join")) {
             const newChannelName = msg.value.split(" ")[1];
             if (newChannelName) {
                 socket.leave(channelName);
                 channelName = newChannelName;
                 socket.join(channelName);
-                socket.emit('joining', channelName);
+                socket.emit('joining channel', channelName);
             }
             console.log('channelName\t', channelName);
 
@@ -60,13 +65,14 @@ io.on('connection', (socket) => {
             io.to(channelName).emit('chat message', fullMessage);
         }
     });
+
 });
 
 
 
 
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server on PORT ${PORT}~~~~`)
 })
 
